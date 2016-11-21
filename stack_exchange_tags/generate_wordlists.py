0da2c12eced9dbc54063aa3bf841747ea4d33b1b
Juan Ignacio Gil Gómez <iwan.manjak@gmail.com>
2016-11-21 18:55:05 +0000

    Solving errors in the test matrix

---
+++ stack_exchange_tags/generate_wordlists.py
@@ -0,0 +1,62 @@
+from bs4 import BeautifulSoup
+import re
+from nltk.corpus import stopwords
+
+
+class GenerateWordLists:
+
+    # cleans title of punctuation and sends to lower case (and optionally, stop words)
+    @staticmethod
+    def titles_to_wordlist(title, remove_stopwords=False):
+        # Function to convert a document to a sequence of words,
+        # optionally removing stop words.  Returns a list of words.
+        # No need to remove HTML
+        #
+        # 1. Remove non-letters (and delete hyphens for now... this will have to be revised later)
+        title_text = re.sub("[^a-zA-Z0-9]", " ", title)
+        #
+        # 2. Convert words to lower case and split them
+        words = title_text.lower().split()
+        #
+        # 3. Optionally remove stop words (false by default)
+        if remove_stopwords:
+            stops = set(stopwords.words("english"))
+            words = [w for w in words if w not in stops]
+        #
+        # 4. Return a list of words
+        return words
+
+    # cleans content of html (and optionally, stop words)
+    @staticmethod
+    def content_to_wordlist(content, remove_stopwords=False):
+        # Function to convert a document to a sequence of words,
+        # optionally removing stop words.  Returns a list of words.
+        #
+        # 1. Remove HTML
+        content_text = BeautifulSoup(content, 'lxml').get_text()
+        #
+        # 2. Remove non-letters (and delete hyphens for now... this will have to be revised later)
+        content_text = re.sub("[^a-zA-Z0-9]", " ", content_text)
+        #
+        # 3. Convert words to lower case and split them
+        words = content_text.lower().split()
+        #
+        # 4. Optionally remove stop words (false by default)
+        if remove_stopwords:
+            stops = set(stopwords.words("english"))
+            words = [w for w in words if w not in stops]
+        #
+        # 5. Return a list of words
+        return words
+
+    # splits tags into list of words
+    @staticmethod
+    def tags_to_wordlist(tags):
+        # Tags are already in lower case and grouped
+        # by using hyphens to incidate multiple words
+        #
+        # 1. Split the tags
+        words = tags.split()
+        #
+        # 2. Return a list of words
+        return words
--- stack_exchange_tags/validate_and_test.py
+++ stack_exchange_tags/validate_and_test.py
@@ -5,7 +5,10 @@ import os
 import csv
 from stack_exchange_tags.naive_bayes import NaiveBayes
 from stack_exchange_tags.iter_message import IterMessage
+from stack_exchange_tags.generate_wordlists import GenerateWordLists as wl
 import numpy as np
+import json
+import scipy.sparse as sp
 
 
 class StackExchangeTags:
@@ -114,7 +117,7 @@ class StackExchangeTags:
         # TODO: Store id in h5 file, so that we don't need the csv
         # Read the csv file
         physics_table = pd.read_csv(test_csv_file, header=0, index_col='id')
-        se_id=physics_table.index
+        se_id = physics_table.index
 
         # Generate train and test sets
         x_train, y_train, x_test = self.tests_sets(train_file=train_file, test_file=test_file)
@@ -147,3 +150,76 @@ class StackExchangeTags:
                 m.print_message(r)
 
             s.close()
+
+    def generate_predictors_test(self, **kwargs):
+
+        hfile = kwargs.get('test_file', self.test_file)
+        test_csv_file = kwargs.get('test_csv_file', self.test_csv_file)
+        test_json_file = kwargs.get('test_json_file', self.test_json_file)
+        train_file = kwargs.get('train_file', self.train_file)
+
+        # Input data files are available in the "../data/" directory.
+        physics_table = pd.read_csv(test_csv_file, header=0, index_col='id')
+
+        print("Cleaning and parsing the training set StackExchange questions...\n")
+
+        # Generate dict with everything
+        data = []
+
+        num_records = physics_table.shape[0]
+        m = IterMessage(num_records, 'processed to json', 1000)
+
+        for record in physics_table.index:
+
+            this_record = {
+                'id': str(record),
+                'topic': 'physics',
+                'title': wl.titles_to_wordlist(physics_table['title'][record], remove_stopwords=True),
+                'content': wl.content_to_wordlist(physics_table['content'][record], remove_stopwords=True),
+                'tags': []
+            }
+
+            data.append(this_record)
+
+            # If the index is evenly divisible by 1000, print a message
+            m.print_message(record)
+
+        # Convert to json
+        with open(test_json_file, 'w') as outfile:
+            json.dump(data, outfile, sort_keys=True, indent=4)
+
+        # Read the data
+        l_data = len(data)
+
+        # List of all possible words in titles and content
+        all_title = dd.io.load(train_file, '/title')
+        all_content = dd.io.load(train_file, '/content')
+
+        l_title = len(all_title)
+        l_content = len(all_content)
+
+        # For each row, generate a vector of binary outputs (one for each tag)
+        # and of binary predictors (one for word in title, one for word in content)
+        predictors = sp.lil_matrix((l_data, l_title + l_content))
+        m = IterMessage(l_data, 'tags generated', 500)
+
+        for x, nd in zip(data, range(l_data)):
+            # for x, nd in zip(data[:100], range(100)):
+
+            for w, n in zip(all_title, range(l_title)):
+                if w in x['title']:
+                    predictors[nd, n] = 1
+
+            for w, n in zip(all_content, range(l_content)):
+                if w in x['content']:
+                    predictors[nd, l_title + n] = 1
+
+            # If the index is evenly divisible by 500, print a message
+            m.print_message(nd)
+
+        dd.io.save(hfile, {
+            'predictors': predictors.tocsr(),
+
+        })
+
+
